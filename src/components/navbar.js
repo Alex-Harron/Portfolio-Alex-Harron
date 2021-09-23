26a73cc04a6689e3bed9fffd7fd035a1998fbe37
Alex-Harron <aharron.harron@gmail.com>
2021-09-23 19:38:27 -0400

    updating

--- src/components/navbar.js
+++ src/components/navbar.js
@@ -8,8 +8,9 @@ class NavBar extends React.Component {
 <div class="topnav">  
   <a href="#home"> <img src={ahlogo} alt='alexharron' className='content' /></a>
   <a href="https://docs.google.com/document/d/1jVgO0CVta1e69ICaEL1YdbwcEIPMHBgUYRPCyUvnjOc/edit?usp=sharing" className='box'>Resume</a>
-  <a href="#news">03. projects</a>
-  <a href="#contact">02. contact</a>
+  <a href="#news">04. contact</a>
+  <a href="#contact">03. projects</a>
+  <a href="#contact">02. experience</a>
   <a href="#about">01. about</a>
 </div>
     )
